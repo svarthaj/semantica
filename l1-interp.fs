082deebd8873a61ce6fa4d888f996b82938ae96d
Lucas Leal <lucas.leal@ufrgs.br>
2016-10-30 09:38:10 -0200

    add rules for applications, let, let rec, and other minor auxiliary functions
    TODO:   - lists
            - exceptions

--- l1-interp.fs
+++ l1-interp.fs
@@ -41,6 +41,46 @@ let isint v =
         | Num(_) -> true
         | _ -> false
 
+(* funcao auxiliar : verifica se é valor *)
+let isvalue v =
+    match v with
+    | Num(_) -> true
+    | Bool(_) -> true
+    | Fn(_,_) -> true
+    | Nil -> true
+    | Raise -> true
+    | _ -> false
+
+(* funcao auxiliar : subsstitui as variaveis livres na expressao *)
+let rec subst (v, x, e) =
+    match e with
+        | Num(n) -> Num(n)
+        | Bool(b) -> Bool(b)
+        | Op(e1, op, e2) -> 
+            let e1' = subst (v, x, e1) in
+                let e2' = subst(v, x, e2) in Op(e1', op, e2')
+        | If(e1, e2, e3) ->
+            let e1' = subst (v, x, e1) in
+                let e2' = subst (v, x, e2) in
+                    let e3' = subst (v, x, e3) in If(e1', e2', e3')
+        | Aplic(e1, e2) ->
+            let e1' = subst(v, x, e1) in
+                let e2' = subst(v,x,e2) in Aplic(e1', e2')
+        | Var(y) -> if y = x then v
+                    else Var(y)  
+        | Fn(y, e1) -> if y = x then e
+                       else let e' = subst(v, x, e1) in Fn(y, e')   
+        | Let(y, e1, e2) -> let e1' = subst(v, x, e1) in
+                                if y = x then Let(y, e1', e2)
+                                else let e2' = subst(v, x, e2) in
+                                        Let(y, e1', e2')
+        | LRec(y, Fn(k, e1), e2) -> if y = x then e
+                                    else let fn' = subst(v, x, Fn(k, e1)) in
+                                            let e2' = subst(v, x, e2) in 
+                                                LRec(y, fn', e2') 
+        | _ -> raise NoRuleApplies
+            
+
 (* Funcao STEP *)
 let rec step t = 
     match t with
@@ -88,6 +128,17 @@ let rec step t =
             let t2' = step t2 in Op(Num(v1), op, t2') 
         | Op(t1, op, t2) ->
             let t1' = step t1 in Op(t1', op, t2) 
+        | Aplic(Fn(var, t1), t2) when isvalue t2 -> subst (t2, var, t1)  
+        | Aplic(Fn(var, t1), t2) ->
+            let t2' = step t2 in Aplic(Fn(var, t1), t2')
+        | Aplic(t1, t2) ->
+            let t1' = step t1 in Aplic(t1', t2)
+        | Let(var, t1, t2) when isvalue t1 -> subst(t1, var, t2)
+        | Let(var, t1, t2) ->
+            let t1' = step t1 in Let(var, t1', t2)
+        | LRec(var1, Fn(var2, t1), t2) -> 
+            let recur = LRec(var1, Fn(var2, t1), t1) in
+                subst(Fn(var2, recur), var1, t2)  
         | _ -> raise NoRuleApplies
 
 (* Implentacao de EVAL *)
@@ -98,30 +149,28 @@ let rec eval t =
 
 
 (* TESTES *)
-let y = Op(Num(17), Sum, Num(9)) in
-    let x = eval y in
-        printfn "%A evaluates to %A" y x 
-
-let y = Op(Op(Num(3), Sum, Num(6)), Mul, Num(9)) in
-    let x = eval y in
-        printfn "%A evaluates to %A" y x 
-
-let y = Op(Op(Num(3), Sum, Num(6)), Mul, Op(Num(9), Sub, Num(4))) in
-    let x = eval y in
-        printfn "%A evaluates to %A" y x 
-
-let y = If(Op(Num(0), Lt, Num(10)), Bool(true), Bool(false)) in
-    let x = eval y in
-        printfn "%A evaluates to %A" y x 
-
-let y = If(Op(Num(10), Leq, Num(4)), Bool(true), Bool(false)) in
-    let x = eval y in
-        printfn "%A evaluates to %A" y x 
-
-let y = If(Op(Num(10), Eq, Num(4)), Bool(true), Bool(false)) in
-    let x = eval y in
-        printfn "%A evaluates to %A" y x 
-
-let y = If(Op(Num(10), Neq, Num(4)), Bool(true), Bool(false)) in
-    let x = eval y in
-        printfn "%A evaluates to %A" y x 
+(* CASOS TESTE *)
+let e1 = Op(Num(17), Sum, Num(9))
+let e2 = Op(Op(Num(3), Sum, Num(6)), Mul, Num(9))
+let e3 = Op(Op(Num(3), Sum, Num(6)), Mul, Op(Num(9), Sub, Num(4)))
+let e4 = If(Op(Num(0), Lt, Num(10)), Bool(true), Bool(false))
+let e5 = If(Op(Num(10), Leq, Num(4)), Bool(true), Bool(false))
+let e6 = If(Op(Num(10), Eq, Num(4)), Bool(true), Bool(false))
+let e7 = If(Op(Num(10), Neq, Num(4)), Bool(true), Bool(false))
+let e8 = Aplic(Fn("x", Var("x")), Num(10))
+let e9 = Aplic(If(Bool(false), Fn("x", Var("x")), Fn("x",Op(Var("x"),Sub,Num(5)))), Num(10))
+let e10 = Let("x", Num(1), Op(Var("x"), Sum, Num(10)))
+let e11 = Let("x", Op(Num(5), Sub, Num(4)), Op(Var("x"), Sub, Num(10)))
+(* exemplo fatorial recursivo *)
+let e12 = LRec(
+            "fat", 
+            Fn( 
+                "x",
+                If(
+                    Op(Var "x", Eq, Num 0),
+                    Num 1,
+                    Op( Var "x", Mul, Aplic(Var "fat", Op(Var "x", Sub, Num 1)))                )
+            ), 
+            Aplic(Var "fat", Num 12))
+(* AVALIAÇÕES *)
+let ev = eval e12 in printfn "%A evaluates to %A" e12 ev
