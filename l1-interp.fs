41e772a889b4295a270cbea4e7c4135d76730009
Lucas Leal <lucas.leal@ufrgs.br>
2016-11-09 20:03:35 -0200

    interpreter finished, only need typeinfer

--- l1-interp.fs
+++ l1-interp.fs
@@ -22,7 +22,6 @@ type term =
     | Fn of variable * term
     | Let of variable * term * term
     | LRec of variable * term * term 
-    | List of term list
     | Cons of term * term
     | Nil
     | Isempty of term
@@ -48,6 +47,7 @@ let isvalue v =
     | Num(_) -> true
     | Bool(_) -> true
     | Fn(_,_) -> true
+    | Cons(_,_) -> true
     | Nil -> true
     | _ -> false
 
@@ -86,9 +86,9 @@ let rec step t =
     match t with
         | If(Bool(true), t2, t3) -> t2
         | If(Bool(false), t2, t3) -> t3
+        | If(Raise, t2, t3) -> Raise
         | If(t1, t2, t3) ->
             let t1' = step t1 in If(t1', t2, t3)
-        | If(Raise, t2, t3) -> Raise
         | Op(Num(v1), Sum, Num(v2)) ->
             let res = v1+v2 in Num(res) 
         | Op(Num(v1), Sub, Num(v2)) ->
@@ -110,8 +110,10 @@ let rec step t =
             let res = (v1 = v2) in Bool(res)
         | Op(Num(v1), Neq, Num(v2)) ->
             let res = (v1 <> v2) in Bool(res)
+        | Op(Num(_), op, Raise) -> Raise
         | Op(Num(v1), op, t2) -> 
             let t2' = step t2 in Op(Num(v1), op, t2') 
+        | Op(Raise, op, t2) -> Raise
         | Op(t1, op, t2) ->
             let t1' = step t1 in Op(t1', op, t2) 
         | Aplic(Fn(var, t1), t2) when isvalue t2 -> subst (t2, var, t1)  
@@ -121,16 +123,34 @@ let rec step t =
         | Aplic(Raise, t2) -> Raise
         | Aplic(t1, t2) ->
             let t1' = step t1 in Aplic(t1', t2)
+        | Let(var, Raise, t2) -> Raise
         | Let(var, t1, t2) when isvalue t1 -> subst(t1, var, t2)
         | Let(var, t1, t2) ->
             let t1' = step t1 in Let(var, t1', t2)
         | LRec(var1, Fn(var2, t1), t2) -> 
             let recur = LRec(var1, Fn(var2, t1), t1) in
                 subst(Fn(var2, recur), var1, t2)  
-        | Try(e1, e2) when isvalue e1 ->    match e1 with
-                                            | Raise -> e2
-                                            | _ -> e1 
+        | Try(Raise, e2) -> e2
+        | Try(e1, e2) when isvalue e1 -> e1 
         | Try(e1, e2) -> let e1' = step e1 in Try(e1',e2)
+        | Cons(e1, e2) when isvalue e1 ->   match e2 with
+                                            | Raise -> Raise
+                                            | _ -> let e2' = step e2 in Cons(e1, e2') 
+        | Cons(Raise, e2) -> Raise
+        | Cons(e1, e2) -> 
+            let e1' = step e1 in Cons(e1',e2)
+        | Hd(Cons(e1,_)) -> e1
+        | Hd(Nil) -> Raise
+        | Hd(Raise) -> Raise
+        | Hd(e1) -> let e1' = step e1 in Hd(e1') 
+        | Tl(Cons(_,e1)) -> e1
+        | Tl(Nil) -> Raise
+        | Tl(Raise) -> Raise
+        | Tl(e1) -> let e1' = step e1 in Tl(e1') 
+        | Isempty(e1) ->    match e1 with
+                            | Nil -> Bool(true)
+                            | Cons(_,_) -> Bool(false)
+                            | _ -> Raise
         | _ -> raise NoRuleApplies
 
 (* Implentacao de EVAL *)
@@ -165,6 +185,14 @@ let e12 = LRec(
             ), 
             Aplic(Var "fat", Num 12))
 let e13 = Try(Op(Num 10, Div, Num 0), Var "err: div by zero")
-let e14 = Try(Op(Num 10, Div, Num 5), Var "err: div by zero")
-(* AVALIAÇÕEiS *)
-let ev = eval e12 in printfn "%A evaluates to %A" e12 ev
+let e14 = Try(Op(Op(Num 10, Div, Num 0), Sum, Num 5), Var "err: div by zero")
+let e15 = Cons(Num 5, Cons(Num 10, Nil))
+let e16 = Hd(Cons(Num 5, Cons(Num 10, Nil)))
+let e17 = Tl(Cons(Num 5, Cons(Num 10, Nil)))
+let e18 = Hd(Nil)
+let e19 = Tl(Nil)
+let e20 = Isempty(Cons(Num 5, Cons(Num 10, Nil)))
+let e21 = Isempty(Nil)
+(* AVALIAÇÕES *)
+let ev1 = eval e20 in printfn "%A evaluates to %A" e20 ev1
+let ev2 = eval e21 in printfn "%A evaluates to %A" e21 ev2
